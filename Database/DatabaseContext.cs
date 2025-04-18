3178f3419234ba2a2ac6f1e1934d837b9aff7cd9
Abdullah Khawahir <abdullah.khawahir@gmail.com>
2025-04-18 14:38:12 +0300

    Add removed User declaration which is declared by Inheritance

--- Database/DatabaseContext.cs
+++ Database/DatabaseContext.cs
@@ -3,7 +3,7 @@ namespace razor.Database;
 public class DatabaseContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
 {
     public DbSet<Attendance> Attendances { get; set; }
-    public DbSet<User> Users { get; set; }
+
     public DatabaseContext(DbContextOptions<DatabaseContext> options)
        : base(options) { }
 }
